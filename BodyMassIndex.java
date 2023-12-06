361d8e2d64a0be759e251520cf71365a4836c5e1
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2023-12-06 20:46:23 +0300

    Vücut kitle indeksi hesaplayan programı yazmış bulunmaktayım.

---
+++ BodyMassIndex.java
@@ -0,0 +1,22 @@
+package patikaodevleri;
+
+import java.util.Scanner;
+
+public class BodyMassIndex{
+
+    public static void main (String[] Args){
+        Scanner input = new Scanner(System.in);
+        double boy;
+        double kilo;
+        System.out.print("Lütfen kilonuzu kilogram cinsinden giriniz: ");
+        kilo = input.nextDouble();
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+        boy = input.nextDouble();
+
+        double index = kilo / (boy * boy);
+        System.out.println("Vücut kitle indexiniz: " + index);
+        input.close();
+    }
+
+}
+
