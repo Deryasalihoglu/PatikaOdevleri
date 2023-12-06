91e8dbb8d26adc2db5c89ff96a58b74bb14e77b3
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2023-12-06 15:26:26 +0300

    git ogreniyorum

---
+++ HipotenusBulanProgram.java
@@ -0,0 +1,21 @@
+package patikaodevleri;
+
+import java.util.Scanner;
+
+class HipotenusBulanProgram {
+
+    public static void main(String[] Args){
+
+        //Uzunlukları a,b ve c(hipotenüs) olan üçgenin hipotenüsü a^2 + b^2 = C^2 formülünden hesaplanır.
+        int a ,b;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Üçgenin ilk kenarını giriniz: ");
+        a = input.nextInt();
+        System.out.print("İkinci kenarı da giriniz: ");
+        b = input.nextInt();
+
+        double c = Math.pow(Math.pow(a, 2) + Math.pow(b,2),0.5);
+        System.out.print("Bu üçgenin hipotenüsü " + c +"'tir.");
+    }
+}
