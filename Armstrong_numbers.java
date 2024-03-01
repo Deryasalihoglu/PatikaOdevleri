3d5d7e29731a52fe17d708131c0cf80eef87d63a
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-03-01 18:57:08 +0300

    Armstrong number assignment is done.

--- Armstrong_numbers.java
+++ Armstrong_numbers.java
@@ -9,25 +9,9 @@ public class Armstrong_numbers {
 
         System.out.println("Enter a number: ");
         int number = input.nextInt();
-        int sameNum= Math.abs(number);
-        //I create a copy of the number because I will use it later and it should stay same. 
-
-        // int q = 0;
-        // do{
-        // number /= 10;
-        // q++;
-        // }while( number != 0 );
-        // System.out.println("basamak sayisi: " + q);
-        // int num2 = 2;
-        // int remainder =1;
-        // int sum = 0;
-        // for(int i = 1; i <= q; i++){
-        // num2 = number /i;
-        // System.out.println("num2: " + num2);
-        // remainder = num2 % 10;
-        // System.out.println("remainder: " + remainder);
-        // sum += remainder;
-        // }
+        int sameNum = Math.abs(number);
+        // I create a copy of the number because I will use it later and it should stay
+        // same.
 
         int quotient;
         int remainder;
@@ -42,18 +26,20 @@ public class Armstrong_numbers {
         } while (quotient > 0);
 
         sameNum = number;
-
-        for(int i = 0; i < counter; i++){
+        // I copied the number to sameNum again
+        
+        for (int i = 0; i < counter; i++) {
             remainder = sameNum % 10;
             sum += Math.pow(remainder, counter);
-            sameNum = sameNum /10;
+            sameNum = sameNum / 10;
         }
 
-        if(sum == number){
+        if (sum == number) {
             System.out.println("Number " + number + " is an Armstrong Number.");
-        }
-        else {
+        } else {
             System.out.println("Number " + number + " is not an Armstrong Number.");
         }
+
+        input.close();
     }
 }
